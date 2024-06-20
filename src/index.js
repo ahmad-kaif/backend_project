6a6fb4ec5d9c97c6d17f7811bc316187859fb101
Ahmad Kaif <heyahmad611@gmail.com>
2024-06-20 19:00:03 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+//whenever connects Db -> always use try catch, async await 
+// DB is always in other continent - always use async await
+// try to use IIFE function (()=>{})()
+
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from './constants';
+import express from 'express';
+const app = express()
+
+// function connectDB(){}
+// connectDB();
+// better approach than this -> using IIFE(immediately invoked function execution) (callback function)()
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+
+
+
+
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error;    
+    }
+})()
+*/
