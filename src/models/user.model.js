c3e6d60bd5b9a2f58b6a749d584f090c6135c669
Ahmad Kaif <heyahmad611@gmail.com>
2024-07-04 17:30:06 +0530

    multer midlleware and cloudinary

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,6 +57,7 @@ userSchema.pre("save", async function(next) {
     this.password = bcrypt.hash(this.password,10)
     next()
 })
+
 userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password,this.password)
 }
@@ -86,6 +87,4 @@ userSchema.methods.generateRefreshToken = function(){
         }
     )
 }
-
-
  export const User = mongoose.model("User",userSchema)
